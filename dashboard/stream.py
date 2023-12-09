9504a5c93640161c9850011873c5dae1fd2bf988
Jeffrey <jeffreyzhan96@gmail.com>
2023-12-10 02:25:24 +0700

    update

--- dashboard/stream.py
+++ dashboard/stream.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-day_df = pd.read_csv('../data/day.csv')
-hour_df = pd.read_csv('../data/hour.csv')
+day_df = pd.read_csv('./data/day.csv')
+hour_df = pd.read_csv('./data/hour.csv')
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
